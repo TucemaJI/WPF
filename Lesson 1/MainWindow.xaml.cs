fe508ef5055a667a9323d1410fc6add3a33a4d62
Simon Timofieiev <bidi75518@gmail.com>
2024-10-03 12:22:33 +0200

    Lesson 1 finished

---
+++ Lesson 1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string TEXT = "Event raised by ";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show(TEXT + sender.ToString());
+            e.Handled = (bool)radioButton1.IsChecked;
+        }
+
+        private void Grid_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show(TEXT + sender.ToString());
+            e.Handled = (bool)radioButton2.IsChecked;
+        }
+
+        private void Window_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show(TEXT + sender.ToString());
+            e.Handled = (bool)radioButton3.IsChecked;
+        }
+    }
+}
