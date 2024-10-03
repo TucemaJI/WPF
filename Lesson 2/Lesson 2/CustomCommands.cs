c9cc2cbc41bad9e935c39a233c84ee251d918853
Simon Timofieiev <bidi75518@gmail.com>
2024-10-03 13:27:42 +0200

    Lesson 2 finished

---
+++ Lesson 2/Lesson 2/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_2
+{
+    public class CustomCommands
+    {
+        private const string NAME = "Launch";
+
+        private static RoutedUICommand launch_command;
+        public static RoutedUICommand Launch { get { return launch_command; } }
+
+        static CustomCommands()
+        {
+            var myInputGestures = new InputGestureCollection { new KeyGesture(Key.L, ModifierKeys.Control) };
+            launch_command = new RoutedUICommand(text: NAME, name: NAME, ownerType: typeof(CustomCommands),
+                                                inputGestures: myInputGestures);
+        }
+    }
+}
---
+++ Lesson 2/Lesson 2/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            var aBinding = new CommandBinding();
+            aBinding.Command = CustomCommands.Launch;
+            aBinding.Executed += new ExecutedRoutedEventHandler(Launch_Handler);
+            aBinding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+            CommandBindings.Add(aBinding);
+        }
+
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Launch Invoked");
+        }
+
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)checkBox.IsChecked;
+        }
+    }
+}
