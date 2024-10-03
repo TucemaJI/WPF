eb69dfcac47a28e546bd8f172a5b85bf9e2a6fb9
Simon Timofieiev <bidi75518@gmail.com>
2024-10-03 17:24:07 +0200

    Lesson 1 finished

---
+++ Lesson 1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Lesson_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Your name is {txtFirstName.Text} {txtLastName.Text} and your email address is {txtEmail.Text}");
+        }
+    }
+}
