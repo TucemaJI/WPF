66af04af86309bd6f9e6abbd121d8c33a9b0be5c
Simon Timofieiev <bidi75518@gmail.com>
2024-10-04 17:37:04 +0200

    Chapter 4 init

---
+++ Lesson 1/Lesson 1/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\username.txt");
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader("C:\\username.txt");
+            label1.Content = "Hello " + sr.ReadToEnd();
+            sr.Close();
+        }
+    }
+}
+
