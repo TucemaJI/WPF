0919f6bcd1f03389ce799b00427fe2547ed1dfc9
Simon Timofieiev <bidi75518@gmail.com>
2024-10-04 17:54:51 +0200

    Lesson 1 finished

--- Lesson 1/Lesson 1/Window1.xaml.cs
+++ Lesson 1/Lesson 1/Window1.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Lesson_1.Properties;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lesson_1
 {
@@ -26,15 +16,15 @@ namespace Lesson_1
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\username.txt");
+            var sw = new StreamWriter(Settings.Default.Path);
             sw.WriteLine(textBox1.Text);
             sw.Close();
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            System.IO.StreamReader sr = new System.IO.StreamReader("C:\\username.txt");
-            label1.Content = "Hello " + sr.ReadToEnd();
+            var sr = new StreamReader(Settings.Default.Path);
+            label1.Content = $"Hello {sr.ReadToEnd()}";
             sr.Close();
         }
     }
