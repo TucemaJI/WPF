8376b41aa0dd97ec7baa00893b01366f93775aba
Simon Timofieiev <bidi75518@gmail.com>
2024-10-06 11:27:19 +0200

    Lesson 2 finished

--- Lesson 2/Lesson 2/Window1.xaml.cs
+++ Lesson 2/Lesson 2/Window1.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Lesson_2
 {
@@ -19,10 +9,37 @@ namespace Lesson_2
     /// </summary>
     public partial class Window1 : Window
     {
-        System.Windows.Forms.OpenFileDialog aDialog = new System.Windows.Forms.OpenFileDialog();
+        OpenFileDialog aDialog = new OpenFileDialog();
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            aDialog.ShowDialog();
+            MediaElement1.Source = new Uri(aDialog.FileName);
+            MediaElement1.Play();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Pause();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Stop();
+        }
+
+        private void MediaElement1_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show($"Media loading unsuccessful. {e.ErrorException.Message}");
         }
     }
 }
