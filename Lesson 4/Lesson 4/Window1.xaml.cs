5599a55e8943ab06971c02551401ddf8e2aaad92
Simon Timofieiev <bidi75518@gmail.com>
2024-10-06 14:41:36 +0200

    Lesson 4 finished

--- Lesson 4/Lesson 4/Window1.xaml.cs
+++ Lesson 4/Lesson 4/Window1.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using Lesson_4.Properties;
 
 namespace Lesson_4
 {
@@ -19,10 +11,48 @@ namespace Lesson_4
     /// </summary>
     public partial class Window1 : Window
     {
-        System.Windows.Forms.OpenFileDialog aDialog = new System.Windows.Forms.OpenFileDialog();
+        OpenFileDialog aDialog = new OpenFileDialog();
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            aDialog.Filter = Settings.Default.Filter;
+            if (aDialog.ShowDialog() is System.Windows.Forms.DialogResult.OK)
+            {
+                Image1.Source = new BitmapImage(new Uri(aDialog.FileName));
+            }
+            FillListBox();
+        }
+
+        private void FillListBox()
+        {
+            if (aDialog.FileName is null || aDialog.FileName == string.Empty)
+            {
+                return;
+            }
+
+            var aDecoder = BitmapDecoder.Create(bitmapUri: new Uri(aDialog.FileName),
+                createOptions: BitmapCreateOptions.None, cacheOption: BitmapCacheOption.Default);
+            var metadata = (BitmapMetadata)aDecoder.Frames[0].Metadata;
+
+            if (metadata is null) { return; }
+
+            if (metadata.Author != null)
+            {
+                ListBox1.Items.Add($"Author: {metadata.Author[0]}");
+            }
+            if (metadata.DateTaken != null)
+            {
+                ListBox1.Items.Add($"Date Taken: {metadata.DateTaken[0]}");
+            }
+            ListBox1.Items.Add($"Rating: {metadata.Rating}");
+            if (metadata.Title != null)
+            {
+                ListBox1.Items.Add($"Title: {metadata.Title}");
+            }
         }
     }
 }
